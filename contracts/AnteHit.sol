b247f05729fc1bbaf6e92c498d346dc2dd60f6d6
Spark <50857240+sparkdoaz@users.noreply.github.com>
2022-08-14 20:09:50 +0800

    feat: 加入遊戲 func

--- contracts/AnteHit.sol
+++ contracts/AnteHit.sol
@@ -7,4 +7,14 @@ contract AnteHit {
 
     event GameStarted(); // 錢都進來了。
     event GameFinished(); 
+    // 前端 call 
+    function joinGame() public payable {
+        require(msg.value == betAmount, "Wrong bet amount.");
+
+        // require(player2.length == 0, "Game has already started.");
+        player1 = msg.sender;
+        player2 = msg.sender;
+        emit GameStarted();
+
+    }
 }
