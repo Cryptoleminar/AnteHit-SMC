5b0a835633bca8183c3963fe2c6212206a1cac5f
Spark <50857240+sparkdoaz@users.noreply.github.com>
2022-08-14 20:47:37 +0800

    other: 增加待辦實現

--- contracts/AnteHit.sol
+++ contracts/AnteHit.sol
@@ -41,6 +41,7 @@ contract AnteHit {
 
     // 後端呼叫來通知遊戲結束
     // 並且發送一個 event 以利後續查詢等等
+    // todo: 需要新增 admin 的 modifier 且 設定為 private
     function gameFinished(address winer, address loser) public {
         // TODO: 需要實作寫入 winner loser
         emit GameFinished();
