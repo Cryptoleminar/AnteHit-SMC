57d411f1de4e3ab9fada1d87f90cbfd23565c4d3
Spark <50857240+sparkdoaz@users.noreply.github.com>
2022-08-14 20:09:28 +0800

    feat: 新增 property 和 事件

--- contracts/AnteHit.sol
+++ contracts/AnteHit.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 contract AnteHit {
+    address public player1;
+    address public player2;
+    uint256 public betAmount = 1;
+
+    event GameStarted(); // 錢都進來了。
+    event GameFinished(); 
 }
